a58a6fb601e07f98a384f7210e6f59cfb5e35923
joselhuidobro <joselhuidobro@gmail.com>
2022-06-24 19:31:37 -0500

    prints on monitor.log

--- logging-client.py
+++ logging-client.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 import zmq
 import time
 import logging
@@ -21,25 +22,36 @@ def recieved():
 
 def log_function(module,timestamp,state,log_stream,log_message):
     
-    logger.info("modulo es ({0}, {1}, {2}, {3} , {4})".format(module,timestamp,state,log_stream,log_message))
- 
+    logger.info("Message data:({0},{1},{2},{3},{4})".format(module,timestamp,state,log_stream,log_message))  
  
 if __name__ == '__main__':
-    #send JSON values to the log function
-    log_func=log_function('a',2,3,4,5)
+ 
     try:
        while True:
          data = recieved()
          json_object = json.loads(data)
+         #json object to list
          dict=json_object['messages']
-         print(dict,type(dict))
-         print(dict[1])
-         
-        # print(json_object['messages']) 
-       
-         #print(json_object,type(json_object))
-         
-         
+         #print(dict,type(dict))
+         #print(dict[1])
+         for i in range(0,len(dict)):
+            print(dict[0])
+            #move in the dictionary
+            a=dict[i]   
+            #give a variable to each data field of the JSON file 
+            module=a['module']
+            timestamp=a['timestamp']
+            state=a['state']
+            log_stream=a['log-stream']
+            log_message=a['log-message']
+            #print to test
+            print("module:", module)
+            print("state:", state)
+            print("timestamp:", timestamp)
+            
+            #send JSON values that now are STRINGS to the log function
+            log_func=log_function(module,timestamp,state,log_stream,log_message)
+                 
       
     except Exception as e:
          logging.exception("Exception occurred")
--- readme.txt
+++ readme.txt
@@ -1,5 +1,18 @@
------------Logging Monitor Task-----------
+Author: José Luis Huidobro Echeverría
 
+-----------Logging Monitor Task-----------
+ Please in a terminal RUN:
+ 
+  python logging-server.python
+ 
+ On another terminal RUN:
+  
+  python logging-client.python
+
+The server is going to send messages to client,
+ all events are saved on the monitor.log file
+
+While both Scripts are running please open the monitor.log file to view events
 
 script "logging-server.py"----------------------
 - Creates a connection between server and client,
@@ -14,3 +27,6 @@ Script "logging-client.py" ---------------------
 - Sends a message back to the server 
 - Prints the recieved JSON data 
 - Creates the file "monitor.log" where activity is being registered, also logs, warning,  
+
+
+Requirements: pip install pyzmq-static
